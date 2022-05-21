833fbc64c0420e9fc094ed3c7434791f8333e641
vicdenys <vicdenys@MacBook-Pro-van-vic.local>
2022-05-21 16:48:43 +0200

    setup tailwind & master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Botellon</title>
+
+
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+        <div class=" container mx-auto items-center">
+            @yield('content')
+        </div>
+
+        
+       
+    </body>
+</html>
