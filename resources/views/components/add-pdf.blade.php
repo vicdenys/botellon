6d2dda88c4ebfde2f82c09e87895bbf49bc0d695
vicdenys <vicdenys@MacBook-Pro-van-vic.local>
2022-05-25 13:23:58 +0200

    file upload wip

--- resources/views/components/add-pdf.blade.php
+++ resources/views/components/add-pdf.blade.php
@@ -19,11 +19,19 @@
 
         <!-- File -->
         <div class="text-center" x-data="{ files: null }">
-            <label class="border-2 relative border-gray-200 hover:bg-gray-100 h-48 text-center p-3 w-full block rounded cursor-pointer my-2" for="customFile2">
-                <input type="file" accept="application/pdf" class="opacity-0 w-full h-full absolute left-0 top-0" id="customFile2" x-on:change="files = Object.values($event.target.files)">
-                <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <span x-text="files ? files.map(file => file.name).join(', ') : 'kies bestand of sleep het hier'"></span>
-                    <button x-show="files" type="reset" @click="files = null" class="absolute flex bg-black  text-white w-4 h-4 align-center justify-center rounded-full">x</button>
+            <label class=" relative border-gray-200 hover:bg-gray-100  text-center w-full block rounded  my-2 " :class="{  'h-72 p-3  cursor-pointer border-2': files === null }" for="customFile2">
+                <input type="file" accept="application/pdf" class="opacity-0 w-full h-full z-50 absolute left-0 top-0" id="customFile2" x-on:change="files = Object.values($event.target.files).length > 0 ? Object.values($event.target.files) : null;">
+                <div class=" w-full ">
+                    <span x-show="!files" x-text="files ? 'bestand geselecteerd' : 'kies bestand of sleep het hier'" :class="{  'text-gray-700': files }"></span>
+                    <div x-show="files" class="relative w-full text-left p-2 mt-4 border bg-gray-200 border-gray-200">
+                        <div class="flex text-left">
+                            <span x-text="files ? files.map(file => file.name).join(', '): ''" class="text-left" :class="{  'text-gray-700': files }"></span>
+                        </div>
+                        <button  x-show="files"  type="reset" @click="files = null" class="absolute  right-0 top-0 flex translate-x-1/2 -translate-y-1/2 bg-black  text-white w-4 h-4 align-center justify-center rounded-full">
+                            <span class="absolute font-sans ">x</span>
+                        </button>
+
+                    </div>
                 </div>
 
             </label>
