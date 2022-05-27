3f07f9796641070d5c6813cd2a70fcd88b69ee8c
vicdenys <vicdenys@MacBook-Pro-van-vic.local>
2022-05-27 16:28:10 +0200

    store file

--- app/Http/Controllers/MenuUploadController.php
+++ app/Http/Controllers/MenuUploadController.php
@@ -4,17 +4,63 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreFileRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\File;
+use Illuminate\Support\MessageBag;
 
 class MenuUploadController extends Controller
 {
-     /**
+    /**
      * Handle an incoming File upload.
      *
      * @param  \App\Http\Requests $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(StoreFileRequest $request)
+    public function store(StoreFileRequest $request,  MessageBag $message_bag)
     {
-        dd($request->all());
+        
+        if ($request->file('menuFile')) {
+            $file = $request->file('menuFile');
+            $filename = time() . '_' . $file->getClientOriginalName();
+
+            // File upload location
+            $location = 'menu-files';
+
+            // Upload file
+            $didFileMove = $file->move($location, $filename);
+
+            if($didFileMove) {
+                
+
+                // Create new file model
+                $file = new File;
+
+                $file->title = $filename;
+
+                if ($request->makeActiveCheckbox){
+                    $fileToChange = File::where('is_active', 1)->update(['is_active' => null]);
+
+                    $file->is_active = true;
+                } else {
+                    $file->is_active = null;
+                }
+                
+
+                $file->save();
+
+                return redirect('/dashboard')->withSuccess('File Uploaded');
+
+
+            } else {
+                return redirect('/dashboard')->withErrors( 'Could not upload file');
+            }
+
+            return redirect('/dashboard')->withSuccess('File Uploaded');
+
+        } else {
+            return redirect('/dashboard')->withErrors( 'Could not upload file');
+        }
+
+        
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    
+
     <div x-data='{{ $errors->any() ? "{open: true}" : "{open: false}"  }}'>
         <x-slot name="header">
             <h2 class="font-semibold  text-gray-800 leading-tight">
@@ -7,9 +7,18 @@
             </h2>
         </x-slot>
 
-        <div class="py-12">
+        <div class="py-6">
 
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                @if (Session::has('success'))
+                <div class="alert alert-danger mb-4" >
+                    <ul class="bg-green-100 p-2 ">
+                        
+                        <li class="text-green-900">{{ Session::get('success') }}</li>
+                        
+                    </ul>
+                </div>
+                @endif
                 <x-button @click="open = true" class="mb-8">
                     {{ __('upload pdf') }}
                 </x-button>
