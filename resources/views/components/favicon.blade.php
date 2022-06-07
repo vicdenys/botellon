402044700f5704af26fc1c147859a213eeec44e7
vicdenys <vicdenys@MacBook-Pro-van-vic.local>
2022-06-07 15:13:20 +0200

    dashboard fixed

--- resources/views/components/favicon.blade.php
+++ resources/views/components/favicon.blade.php
@@ -2,6 +2,6 @@
 <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
 <link rel="manifest" href="/img/favicon/site.webmanifest">
-<link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#000000">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
