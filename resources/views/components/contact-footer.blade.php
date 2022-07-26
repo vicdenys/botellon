aeff492caf1c438282608b0ba736f56a8c4ef44e
vicdenys <vicdenys@MacBook-Pro-van-vic.local>
2022-07-26 18:30:37 +0200

    wip

--- resources/views/components/contact-footer.blade.php
+++ resources/views/components/contact-footer.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center  border-t border-green text-sm md:text-lg  bg-white">
-    <div class="border-b md:border-b-0 border-green py-4 px-8 flex items-center md:items-end justify-between">
-        <div>
+    <div class="border-b md:border-b-0 border-green py-4 px-8 grid grid-cols-2 md:grid-cols-3 items-center md:items-end justify-between">
+        <div class="text-left">
             <p class="block">
                 Waalsekaai 45a
             </p>
@@ -13,12 +13,12 @@
                 © 2022 botellón Antwerp
             </p>
         </div>
-        <div>
-            <a href="https://www.instagram.com/botellon_antwerp/" target="_blank" class=" flex items-center gap-1" data-hover>
+        <div class="text-right">
+            <a href="https://www.instagram.com/botellon_antwerp/" target="_blank" class=" flex items-center justify-end gap-1" data-hover>
                 <img class="inline-block w-[0.65rem]" src="img/instagram-icon.svg" alt="instagram icon">
                 Instagram
             </a>
-            <a href="https://www.facebook.com/botellonantwerp" target="_blank" class=" flex items-center gap-1" data-hover>
+            <a href="https://www.facebook.com/botellonantwerp" target="_blank" class=" flex items-center justify-end gap-1" data-hover>
                 <img class="inline-block w-[0.65rem]" src="img/facebook-icon.svg" alt="facebook icon">
                 Facebook
             </a>
